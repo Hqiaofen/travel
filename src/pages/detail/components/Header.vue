d0ba358220c6d20433f15370f6c9fcd63770f9e2
Hqiaofen <1097044299@qq.com>
2021-04-09 15:00:56 +0800

    finish detailHeader

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,17 +1,52 @@
 <template>
   <div>
-    <div class="header-abs"></div>
-    <div class="header-fixed"></div>
+    <router-link tag="div" to="/" class="header-abs"
+    v-show="showAbs">
+      <span class="iconfont header-abs-back">&#xe624;</span>
+    </router-link>
+    <div class="header-fixed"
+    v-show="!showAbs"
+    :style="opacityStyle">
+      <router-link to="/">
+        <span class="iconfont header-fixed-back" >&#xe624;</span>
+      </router-link>
+      景点详情
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DetailHeader'
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacityStyle: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .header-abs
     position: absolute
     left: .2rem
@@ -19,6 +54,28 @@ export default {
     width: .8rem
     height: .8rem
     border-radius: .4rem
+    line-height: .8rem
+    text-align: center
     background: rgba(0, 0, 0 , .5)
-
+    .header-abs-back
+      color #fff
+      font-size: .4rem
+  .header-fixed
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+    .header-fixed-back
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
 </style>
