d71bbaec077139599ca4a6ef9009cb0616d65fd1
Hqiaofen <1097044299@qq.com>
2021-04-11 00:14:35 +0800

    fix detail-header

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -39,10 +39,10 @@ export default {
       }
     }
   },
-  activated () {
+  mounted () {
     window.addEventListener('scroll', this.handleScroll)
   },
-  deactivated () {
+  unmounted () {
     window.removeEventListener('scroll', this.handleScroll)
   }
 }
